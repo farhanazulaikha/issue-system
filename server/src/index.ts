b31dc7dfe4efa9e8502e4ef2064708786a5e48c7
farhanazulaikha <farhanazulaikha499@gmail.com>
2024-03-09 14:53:20 +0800

    remove unused code

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,26 +11,7 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.use('/user', userRoutes);// This means all route path preceed this path
-
-// app.get('/', (req, res) => {
-//     res.send('aku bosan')
-// })
-
-// app.get('/health', (req, res) => {
-//     res.send(`ok`)
-// })
-
-// app.get('/try', (req, res) => {
-//     res.send(`try`)
-// })
-
-// app.get('/:id', (req, res) => {
-//     console.log(`Query parameter: ${JSON.stringify(req.query)}`);
-//     console.log(`Headers: ${JSON.stringify(req.headers)}`);
-//     console.log(`Method: ${req.method}`);
-//     res.status(200).header({'X-Custom-Header': 'foo', 'X-Custom-Header-2': 'fuu'}).send(`iya, ${req.params.id}`)
-// })
+app.use('/user', userRoutes);
 
 app.listen(port, () => {
     console.log(`Listening to port ${port}`)
