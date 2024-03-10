3e1ad5431c79e555f0541d322537cc59fb986b74
farhanazulaikha <farhanazulaikha499@gmail.com>
2024-03-10 22:36:03 +0800

    trim input before checking empty

--- server/src/middleware/request-validation.middleware.ts
+++ server/src/middleware/request-validation.middleware.ts
@@ -1,16 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
 
 export function validateIssueRequest(req: Request, res: Response, next: NextFunction) {
-    req.check('name', 'Please enter a name').notEmpty(),
+    req.check('name', 'Please enter a name').trim().notEmpty(),
     req.check('name', 'Please enter a name in string format').isString(),
-    req.check('email', 'Please enter an email').notEmpty(),
+    req.check('email', 'Please enter an email').trim().notEmpty(),
     req.check('email', 'Please enter an email in string format').isString(),
     req.check('email', 'Please enter a valid email').isEmail(),
-    req.check('selectedCategory', 'Please choose/enter a category').notEmpty(),
+    req.check('selectedCategory', 'Please choose/enter a category').trim().notEmpty(),
     req.check('selectedCategory', 'Please choose/enter a category in string').isString(),
-    req.check('selectedSubCategory', 'Please choose/enter a subcategory').notEmpty(),
+    req.check('selectedSubCategory', 'Please choose/enter a subcategory').trim().notEmpty(),
     req.check('selectedSubCategory', 'Please choose/enter a subcategory in string').isString(),
-    req.check('description', 'Please enter a description').notEmpty(),
+    req.check('description', 'Please enter a description').trim().notEmpty(),
     req.check('description', 'Please enter a description in string').isString(),
     next();
 }
